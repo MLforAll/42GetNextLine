6c8bc5f2e238acf16e95217e1c2917d14dd2a1f4
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-01-27 22:02:20 +0100

    Updates

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 18:41:33 by kdumarai          #+#    #+#             */
-/*   Updated: 2017/12/16 17:19:49 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/01/27 22:01:45 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "Libft/libft.h"
+# include "libft.h"
 # define BUFF_SIZE	8
 
 int					get_next_line(const int fd, char **line);
